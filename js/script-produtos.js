e8e8b17646c1aaaf4cceb213f47aac2ca3474395
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-05-10 10:47:29 -0300

    Feito alguns ajustes

--- js/script-produtos.js
+++ js/script-produtos.js
@@ -1,14 +1,26 @@
 // Filtrar os shapes pela modalidade
-filtro = document.querySelectorAll('.produtos-filtro .filtro button')
-
+botaoFiltro = document.querySelectorAll('.produtos-filtro .filtro button')
 shapes = document.querySelectorAll('.produtos-shapes li')
 
 function executarFiltro(event) {
   elemento = event.currentTarget;
   classFiltro = elemento.getAttribute('class')
-  
+
+  // Pintar o botão quando clicado
+  for (let i = 0; i < botaoFiltro.length; i++) {
+
+    if(botaoFiltro[i] === elemento) {
+      botaoFiltro[i].style.color = "#FFFFFF";
+      botaoFiltro[i].style.background = "#7D3F92";
+    }
+  }
+
+  // Buscar um jeito de fazer com que o botao anterior que foi selecinado
+  // Volte para sua cor inicial
+
+  // Mostrar os shapes do filtro selecionado
   for (let i = 0; i < shapes.length ; i++) {
-    
+
     if(shapes[i].classList.contains(classFiltro)) {
       shapes[i].classList.add('ativo') 
 
@@ -22,5 +34,5 @@ function cliqueFiltro(clique) {
 clique.addEventListener('click', executarFiltro)
 }
 
-filtro.forEach(cliqueFiltro);
+botaoFiltro.forEach(cliqueFiltro);
 
