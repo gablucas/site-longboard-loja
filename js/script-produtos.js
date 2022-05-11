6e5475acb67aab61f24a92c2610239a3accdb03f
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-05-11 17:47:11 -0300

    Adicionado nova funcao no filtro, hover nos links do menu secundario e ajuste do alinhamento dos titulos dos filtros

--- js/script-produtos.js
+++ js/script-produtos.js
@@ -1,38 +1,58 @@
+// Mantem selecionado a seção de produtos no menu secundario
+menuSecundario = document.querySelectorAll('.menu-secundario a')
+
+function ativarMenuSecundario(link) {
+  href = link.href;
+  url = location.href;
+
+  if(url.includes(href)) {
+    link.classList.add('ativo')
+  }
+}
+
+menuSecundario.forEach(ativarMenuSecundario)
+
 // Filtrar os shapes pela modalidade
 botaoFiltro = document.querySelectorAll('.produtos-filtro .filtro button')
 shapes = document.querySelectorAll('.produtos-shapes li')
 
 function executarFiltro(event) {
-  elemento = event.currentTarget;
-  classFiltro = elemento.getAttribute('class')
+  elemento = event.currentTarget; // Armazena o elemento clicado na variavel
+  classFiltro = elemento.getAttribute('class'); // Armazena a(s) classe(s) do elemento clicado na variavel
+
 
   // Pintar o botão quando clicado
   for (let i = 0; i < botaoFiltro.length; i++) {
 
-    botaoFiltro[i].classList.remove('selecionado')
+    if(botaoFiltro[i].classList.contains('selecionado')) {
+      botaoFiltro[i].classList.remove('selecionado')
 
-    if(botaoFiltro[i] === elemento) {
-      botaoFiltro[i].classList.add('selecionado');
+    } else if(botaoFiltro[i] === elemento) {
+        botaoFiltro[i].classList.add('selecionado')
     }
   }
 
-  // Buscar um jeito de fazer com que o botao anterior que foi selecinado
-  // Volte para sua cor inicial
-
   // Mostrar os shapes do filtro selecionado
   for (let i = 0; i < shapes.length ; i++) {
+    shapes[i].classList.add('ativo');
 
-    if(shapes[i].classList.contains(classFiltro)) {
-      shapes[i].classList.add('ativo') 
-
+    // Se o botão for desselecionado, mostra todos os shapes
+    if(elemento.classList.contains('selecionado') === false) {
+      shapes[i].classList.add('ativo');
+      
+    // Filtra e exibe os shapes de acordo com o filtro selecionado
+    } else if(shapes[i].classList.contains(classFiltro)) {
+      shapes[i].classList.add('ativo');
+      
+    // Remove todos os shapes que não estão de acordo com o filtro
     } else {
-        shapes[i].classList.remove('ativo')
+      shapes[i].classList.remove('ativo');
     }
   }
 }
 
 function cliqueFiltro(clique) {
-clique.addEventListener('click', executarFiltro)
+  clique.addEventListener('click', executarFiltro)
 }
 
 botaoFiltro.forEach(cliqueFiltro);
