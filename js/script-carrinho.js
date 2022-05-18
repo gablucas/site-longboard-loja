c84a6d6995f1e7b1b2cf184b56c1c91b40a0372f
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-05-18 17:57:03 -0300

    Adicionado nova funcionalidade nos radio buttons de frete

--- js/script-carrinho.js
+++ js/script-carrinho.js
@@ -1,10 +1,10 @@
 //Ativar o endereço selecionado
 endereco = document.querySelectorAll('.carrinho-endereco-item');
+enderecoInput = document.querySelectorAll('.carrinho-endereco-item input');
 
 function adicionarChecked(event) {
   enderecoSelecionado = event.currentTarget;
-  enderecoInput = document.querySelectorAll('.carrinho-endereco-item input');
-
+  
   for(i = 0; i < endereco.length; i++) {
 
     if(enderecoSelecionado.contains(enderecoInput[i])) {
@@ -17,18 +17,52 @@ function adicionarChecked(event) {
   }
 }
 
-function cliqueInput(item) {
+function cliqueEndereco(item) {
   item.addEventListener('click', adicionarChecked)
 }
 
-endereco.forEach(cliqueInput)
+endereco.forEach(cliqueEndereco)
+
+// Ativar o frete selecionado
+freteLabel = document.querySelectorAll('.carrinho-frete label')
+freteInput = document.querySelectorAll('.carrinho-frete input')
+
+function selecionarFrete(event) {
+  freteSelecionado = event.currentTarget;
+
+  for(i = 0; i < freteLabel.length; i++) {
+    if(freteSelecionado === (freteLabel[i])) {
+      freteInput[i].checked = true;
+    }
+  }
+}
+
+function cliqueFrete(item) {
+  item.addEventListener('click', selecionarFrete)
+}
+
+freteLabel.forEach(cliqueFrete)
 
 // Ativar pagamento
 function ativaPagamento() {
-  carrinhoEndereco = document.querySelector('.carrinho-entrega > div')
-  carrinhoEndereco.classList.remove('ativo')
-  console.log(carrinhoEndereco)
+  error = document.querySelector('.error')
 
-  carrinhoPagamento = document.querySelector('.carrinho-pagamento > div')
-  carrinhoPagamento.classList.add('ativo')
+  // Verificar se há um endereço e frete selecionado
+  for(i = 0; i < endereco.length; i++) {
+
+    // Se sim, finaliza a aba de endereço e abre a de pagamento
+    if(enderecoInput[i].checked && freteInput[i].checked) {
+      carrinhoEndereco = document.querySelector('.carrinho-entrega > div');
+      carrinhoEndereco.classList.remove('ativo');
+    
+      carrinhoPagamento = document.querySelector('.carrinho-pagamento > div');
+      carrinhoPagamento.classList.add('ativo');
+
+    // Se não, exibe uma mensagem de erro
+    } else {
+      error.textContent = "Por favor, selecione um endereço";
+      error.style.color = "red";
+      error.style.textAlign = "center";
+    }
+  }
 }
