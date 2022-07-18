e027e29bf2e0a96aba635565824d663440a37e4c
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-07-18 13:42:08 -0300

    Atualizado alguns links

--- js/dropdown.js
+++ js/dropdown.js
@@ -8,7 +8,6 @@ dropdown.forEach((menu) => {
 })
 
 function handleClick(e) {
-    e.preventDefault();
     this.classList.add('active');
     outsideClick(this, ()=> {
         this.classList.remove('active')
