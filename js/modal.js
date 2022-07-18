f8273a33489e9705a1b7aeb88832e943261b1964
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-07-18 15:50:24 -0300

    Iniciado a construcao do modal

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector('[data-modal]');
+const modalFechar = document.querySelector('[data-modal="fechar"]');
+const modalContainer = document.querySelector('[data-modal="container"]');
+
+
+modal.addEventListener('click', ativarModal);
+
+function ativarModal(e) {
+  e.preventDefault();
+  modalContainer.classList.add('ativo');
+
+  modalContainer.addEventListener('click', clickOutside)
+}
+
+function clickOutside(e) {
+  if(e.target === modalContainer) {
+    desativarModal();
+  }
+}
+
+modalFechar.addEventListener('click', desativarModal)
+
+function desativarModal() {
+  modalContainer.classList.remove('ativo')
+}
+
