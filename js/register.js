734990ae2e81e46dadd30862c92e57b645c36796
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-07-25 11:23:18 -0300

    Atualizado o registro do site

--- js/register.js
+++ js/register.js
@@ -1,4 +1,3 @@
-
 const registerStep1 = document.querySelector('[data-submit="register-step1"]');
 const registerStep2 = document.querySelector('[data-submit="register-step2"]');
 const registerStep3 = document.querySelector('[data-submit="register-step3"]');
@@ -16,6 +15,7 @@ const confirmPassword = document.querySelector('[data-register="confirm-password
 const confirmPasswordError = document.querySelector('[data-error="confirm-password"]');
 
 const registerButtonStep1 = document.querySelector('[data-register="button-step1"]');
+
 
 registerButtonStep1.addEventListener('click', validateRegisterStep1)
 
@@ -41,3 +41,38 @@ function validateRegisterStep1(e) {
 }
 
 // Validacao registro - Etapa 2
+const registerName = document.querySelector('[data-register="name"]');
+const registerNameError = document.querySelector('[data-error="register-name"]');
+
+const registerLastName = document.querySelector('[data-register="lastname"]');
+const registerLastNameError = document.querySelector('[data-error="register-lastname"]');
+
+const registerCPF = document.querySelector('[data-register="cpf"]');
+const registerCPFError = document.querySelector('[data-error="register-cpf"]');
+
+
+const registerButtonStep2 = document.querySelector('[data-register="button-step2"]');
+
+registerButtonStep2.addEventListener('click', validateRegisterStep2)
+
+function validateRegisterStep2(e) {
+  registerNameError.innerHTML = "";
+  registerLastNameError.innerHTML = "";
+  registerCPFError.innerHTML = "";
+
+  if(!registerName.value) {
+    e.preventDefault();
+    registerNameError.innerHTML = "Digite seu nome";
+  }else if(!registerLastName.value) {
+    e.preventDefault();
+    registerLastNameError.innerHTML = "Digite seu sobrenome";
+  }else if(!registerCPF.value) {
+    e.preventDefault();
+    registerCPFError.innerHTML = "Digite seu CPF";
+  } else {
+    registerStep2.classList.remove('active');
+    registerStep3.classList.add('active');
+    stepCount.innerHTML = 3;
+  }
+}
+
