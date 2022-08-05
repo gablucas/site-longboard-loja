bbbc2b28b9ef56c35a1603307006974fa7d596a0
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-08-04 23:09:38 -0300

    Adicionado codigo que acessa as paginas sem recarrega-las

--- js/home.js
+++ js/home.js
@@ -2,8 +2,10 @@ import initModal from "./modules/modal.js";
 import initDropDown from "./modules/dropdown.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initHomeFilter from "./modules/home-filter.js";
+import changePage from "./modules/change-page.js";
 
 initModal();
 initDropDown();
 initMenuMobile();
 initHomeFilter();
+changePage();
