bad979b5db77e7e41a7641712cccb5728ce69984
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-05 14:42:01 -0300

    Refatorado o codigo do filtro de shapes da home

--- js/modules/home-filter.js
+++ js/modules/home-filter.js
@@ -1,25 +1,29 @@
 export default function initHomeFilter() {
 /** EXIBIR OS SHAPES NA HOME DE ACORDO COM A MODALIDADE SELECIONADA */
-const modalidades = document.querySelectorAll('.home-shapes-modalidades input')
+const modalidades = document.querySelectorAll('.home-shapes-modalidades button')
 const shapes = document.querySelectorAll('.home-produtos-shapes li');
-
-// Nova forma
-function selecionarShapes(event) {
-  const modalidadeID = event.currentTarget.getAttribute('id');
-  
-  shapes.forEach((item) => {
-    if(item.classList.contains(modalidadeID)) {
-      item.classList.add('ativo');
-    } else {
-      item.classList.remove('ativo');
-    }
-  })
-}
 
 modalidades.forEach((modalidade) => {
   modalidade.addEventListener('click', selecionarShapes)
 })
 
-  // Selecionado o item
-  const inputSelecionado =  document.querySelector('.home-shapes-modalidades input[id=dancingfreestyleHome]').checked = true;
+function selecionarShapes(e) {
+  const modalidadeID = e.currentTarget.getAttribute('id');
+  
+  modalidades.forEach((btn) => {
+    if(btn === e.currentTarget) {
+      btn.classList.add('active');
+    } else {
+      btn.classList.remove('active')
+    }
+  })
+
+  shapes.forEach((item) => {
+    if(item.classList.contains(modalidadeID)) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  })
+}
 }
