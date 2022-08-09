b9aa319206633479cd92bce44d0aac3a6f08b6e5
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-09 14:03:27 -0300

    Atualizado o codigo de registrar o usuario, registrar no localStorage

--- js/modules/register/registerUser.js
+++ js/modules/register/registerUser.js
@@ -1,18 +1,20 @@
 import {account} from "../users.js";
 
 export default function registerUser(forms) {
-  const user = {};
+  const user = {endereco: {}};
   forms.forEach((form) => {
     Array.from(form).forEach((input) => {
       if(input.name === "confirmar-senha") {
-      } else {
+      }else if(input.name === "cep" || input.name === "rua" || input.name === "numero" || input.name === "complemento" || input.name === "cidade" || input.name === "bairro" || input.name === "estado") {
+        user.endereco[input.name] = input.value;
+      }else{
         user[input.name] = input.value;
       }
     })
   })
   account.push(user)
   console.log(account)
-  localStorage.teste = JSON.stringify(account[1])
+  localStorage[account[account.length - 1].cpf] = JSON.stringify(account[account.length - 1])
 }
 
 
