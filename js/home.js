21d2b05ded5ff07ab6c9ebb8d75d0ce10b9c397f
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-19 14:53:03 -0300

    Adicionado codigo que mostra cada shape em uma pagina so, sem a necessidade de ter um html para cada shape

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ import initDropDown from "./modules/global/dropdown.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initHomeFilter from "./modules/home-filter.js";
 import loggedUser from "./modules/global/loggedUser.js";
-import showProducts from "./modules/products/showProducts.js";
+import showProducts from "./modules/produtos/showProducts.js";
 
 initDropDown();
 initMenuMobile();
--- js/modules/produto/showProduct.js
+++ js/modules/produto/showProduct.js
@@ -1,7 +1,40 @@
-export default function showProduct() {
-  const dataProduct = document.querySelectorAll('[data-product]');
+const dataProduct = document.querySelectorAll('[data-product]');
+
+export default async function showProduct() {
   const parametros = new URLSearchParams(location.search);
-  parametros.forEach((teste) => {
-    console.log(teste)
+  const typeProduct = parametros.get('type');
+  const idProduct = parametros.get('id');
+  
+  const promise = await fetch('../../../products.json')
+  const productsJson = await promise.json();
+  const product = productsJson[typeProduct].find(product => product.id === idProduct);
+
+  dataProduct.forEach((element) => {
+    const attribute = element.getAttribute('data-product');
+
+    if(attribute.includes("dimensions")) {
+      const dimensionType = attribute.split("-")[1];
+      element.innerText = product.dimensions[dimensionType];
+
+    }else if(attribute.includes("longometer")){
+      const longometerType = attribute.split("-")[1];
+      element.classList.add(`nota${product.longometer[longometerType]}`);
+
+    }else if(attribute.includes('images')){
+      product.images.forEach((image) => {
+        element.innerHTML += `<img src="../../${image}" alt=""></img>`;
+      })
+
+      const mainImage = document.querySelector('[data-product="main-image"]');
+      mainImage.setAttribute('src', `../../${product.images[0]}`);
+
+    }else if(attribute.includes("flex")){
+      product.flex.forEach((flex) => {
+        element.innerHTML += `<option value="${flex.replace(" ","").toLowerCase()}">${flex}</option>`;
+      });
+      
+    }else{
+      element.innerText = product[attribute];
+    }
   })
 }
