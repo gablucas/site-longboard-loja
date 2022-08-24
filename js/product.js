fdacd40c6c7b7bee3e19d305bf44dbbd75985bef
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-24 16:31:29 -0300

    Mais ajustes feitos nas paginas html

--- js/product.js
+++ js/product.js
@@ -1,9 +1,11 @@
+import initDropDown from "./modules/global/dropdown.js"
 import loggedUser from "./modules/global/loggedUser.js";
 import exitAccount from "./modules/global/exitAccount.js";
 import showProduct from "./modules/produto/showProduct.js";
 import addItemCart from "./modules/produto/addItemCart.js";
 import showQuantityItensCart from "./modules/global/showQuantityItensCart.js"
 
+initDropDown();
 loggedUser();
 exitAccount();
 showProduct();
