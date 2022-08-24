ea2825e70fb11061b528dec7bf96baffcb374b1d
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-08-24 13:51:04 -0300

    Ajuste no diretorio dos arquivos das paginas dos produtos

--- js/modules/produtos/showProducts.js
+++ js/modules/produtos/showProducts.js
@@ -11,12 +11,12 @@ export default async function showProducts() {
     
     // Mostra os shapes da aba home
     if(productType === "shapes" && list.getAttribute('class').includes("home")) {
-      list.innerHTML += `<li class="${product.modality} active"><a href="../../produto/shape.html?type=shapes&id=${product.id}"><img src="./${product.images[0]}" alt="" width="360" height="360"><h3 class="font-1-s cor-10">${product.name}</h3></a>
+      list.innerHTML += `<li class="${product.modality} active"><a href="../produto/shape.html?type=shapes&id=${product.id}"><img src="./${product.images[0]}" alt="" width="360" height="360"><h3 class="font-1-s cor-10">${product.name}</h3></a>
       </li>`;
 
     // Mostra os shapes da aba produtos
     } else if(productType === "shapes") {
-      list.innerHTML += `<li class="${product.modality} ${product.mount} tamanho4 ativo"><a href="../../produto/shape.html?type=shapes&id=${product.id}"><img src="../../${product.images[0]}" alt="" width="260" height="260"><h3 class="font-1-s cor-10">${product.name}</h3></a>
+      list.innerHTML += `<li class="${product.modality} ${product.mount} tamanho4 ativo"><a href="../produto/shape.html?type=shapes&id=${product.id}"><img src="../${product.images[0]}" alt="" width="260" height="260"><h3 class="font-1-s cor-10">${product.name}</h3></a>
       </li>`;
     } else {
       console.log('precisa adicionar estes produtos')
