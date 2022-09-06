16b0fbee118463aecf0aac2514a15739c019cfbc
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-09-06 20:02:02 -0300

    Ajustes

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 import loggedUser from "./modules/global/loggedUser.js";
 import exitAccount from "./modules/global/exitAccount.js";
-import dropDown from "./modules/global/dropDown.js";
+import dropDown from "./modules/global/dropdown.js";
 import menuMobile from "./modules/global/menuMobile.js";
 import homeFilter from "./modules/home/filterShapesHome.js";
 import showProducts from "./modules/produtos/showProducts.js";
