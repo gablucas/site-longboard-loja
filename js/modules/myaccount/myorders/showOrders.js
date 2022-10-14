67b2cf226cef2883ccc4fa6733e71580590c569c
Gabthedeveloper <gabthedeveloper@gmail.com>
2022-10-14 11:03:00 -0300

    finalizado o codigo que mostra os detalhes do pedido

--- js/modules/myaccount/myorders/showOrders.js
+++ js/modules/myaccount/myorders/showOrders.js
@@ -1,12 +1,14 @@
 import { accounts } from "../../global/accounts.js";
+import fetchProducts from "../../global/fetchProducts.js";
+import { ElementHandler } from "../../global/elementHandler.js";
 
 const container = document.querySelector('[data-myorders="container"]');
 const orders = accounts.loggedUser().pedidos;
+const elementHandler = new ElementHandler()
 
 export function showOrders() {
   orders.forEach((order) => {
     const div =  document.createElement('div');
-
     div.classList.add('order');
 
     div.innerHTML = `
@@ -30,8 +32,40 @@ export function showOrders() {
       <span class="font-2-xxs">${order[0].orderState}</span>
     </div>
     
-    <div class='details'></div>`
+    <div class='details' data-hide></div>`
 
     container.appendChild(div);
   })
+
+  const ordersElement = document.querySelectorAll('.order');
+
+  function insertDetails(element, index) {
+    const detail = element.lastElementChild;
+    const order = accounts.loggedUser().pedidos[index];
+    order.shift();
+
+    fetchProducts((products) => {
+      order.forEach((product) => {
+        const selectedProduct = products[product.type].find((item) => item.id === product.id);
+        const div = document.createElement('div');
+
+        div.classList.add('product');
+       
+        div.innerHTML = `<a href=""><img src="../${selectedProduct.images[0]}" alt=""></a>
+        <span>${selectedProduct.name}</span>
+        <span>${product.flex}</span>
+        <span>R$ ${selectedProduct.price},00</span>
+        <span>${product.quantity}</span>
+        <span>R$ ${selectedProduct.price * product.quantity},00</span>`;
+        detail.appendChild(div);
+      })
+    })
+
+    element.addEventListener('click', ({currentTarget}) => {
+      const detailElement = currentTarget.lastElementChild;
+      detailElement.toggleAttribute('data-hide')
+    })
+  }
+
+  ordersElement.forEach(insertDetails)
 }
