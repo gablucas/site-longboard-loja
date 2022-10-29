2fd294fd01431487c66452659a4d38c762faeae7
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-10-29 01:33:01 -0300

    Ajustado a responsividade da pagina login.html e register.html

--- js/login.js
+++ js/login.js
@@ -1,8 +1,5 @@
-import showQuantityItensCart from "./modules/global/showQuantityItensCart.js";
 import { validateLogin } from "./modules/global/validateLogin.js";
 
-
-showQuantityItensCart();
 validateLogin();
 
 
